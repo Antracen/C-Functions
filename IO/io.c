b6dd7418e1ccafa777987756787f41e7bb908765
Antracen <martin.wass@hotmail.com>
2017-11-11 16:45:52 +0100

    Fix indentation.

--- IO/io.c
+++ IO/io.c
@@ -3,7 +3,6 @@
 // Scan a string from stdin and return the length.
 int scanstring(char* str){
 	char c = getchar_unlocked();
-	while(c < 48 || c > 57) c = getchar_unlocked();
 	int i = 0;
 	while(c != '\n'){
 		str[i] = c;
@@ -17,12 +16,12 @@ int scanstring(char* str){
 // Scan an int into the adress in function argument.
 void scanint(int* num){
 	char c = getchar_unlocked();
-    *num = 0;
-    while(c < 48 || c > 57) c = getchar_unlocked();
-    while(47 < c && c < 58){
-        *num = *num*10 + c - 48;
-        c = getchar_unlocked();
-    }
+	*num = 0;
+	while(c < 48 || c > 57) c = getchar_unlocked();
+	while(47 < c && c < 58){
+		*num = *num*10 + c - 48;
+		c = getchar_unlocked();
+	}
 }
 
 int main(){
